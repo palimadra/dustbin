ca9967d1e32418762e7e216a8fe1921ba7d524b9
= <sean.fioritto@gmail.com>
2012-12-08 14:32:07 -0600

    auth is stubbed in but not implemented.

--- api/app.py
+++ api/app.py
@@ -12,13 +12,10 @@ class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
             (r"/", HomeHandler),
-            (r"/auth/login", AuthLoginHandler),
-            (r"/auth/logout", AuthLogoutHandler),
             (r"/test", TestHandler)
         ]
         settings = dict(
             cookie_secret="^gh\x06t\x08\xd8m2\x01\xf83\xfeu\xd3\xa9I\xcc6\x8d",
-            login_url="/auth/login",
             autoescape=None,
         )
         tornado.web.Application.__init__(self, handlers, **settings)
@@ -47,34 +44,6 @@ class TestHandler(BaseHandler):
     @tornado.web.authenticated
     def get(self):
         self.write("blah")
-
-
-class AuthLoginHandler(BaseHandler, tornado.auth.GoogleMixin):
-    @tornado.web.asynchronous
-    def get(self):
-        if self.get_argument("openid.mode", None):
-            self.get_authenticated_user(self.async_callback(self._on_auth))
-            return
-        self.authenticate_redirect()
-
-    def _on_auth(self, user):
-
-        if not user:
-            raise tornado.web.HTTPError(500, "Google auth failed")
-        email = user["email"].encode("utf-8")
-        name = self.db.get(email)
-        if not name:
-            name = user["name"].encode("utf-8")
-            self.db.set(email, name)
-
-        self.set_secure_cookie("user", email)
-        self.redirect(self.get_argument("next", "/"))
-
-
-class AuthLogoutHandler(BaseHandler):
-    def get(self):
-        self.clear_cookie("user")
-        self.redirect(self.get_argument("next", "/"))
 
 
 def main():
